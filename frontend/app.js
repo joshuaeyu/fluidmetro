e591a2bc7fa6870c5c7cf78cd3c729d3f6ec410b
Joshua Yu <joshuayu12@gmail.com>
2026-01-06 23:16:46 -0800

    plot vehicles as points on canvas

--- frontend/app.js
+++ frontend/app.js
@@ -1,20 +1,49 @@
 "use strict";
 
-// Data refresh button
+// Document elements
 const refreshBtn = document.getElementById("refresh-btn");
 const vehiclesDiv = document.getElementById("vehicles");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 refreshBtn.addEventListener("click", fetchData);
+
+// San Francisco border coordinates
+const NORTH_BORDER = 37.833;
+const SOUTH_BORDER = 37.708;
+const EAST_BORDER = -122.359;
+const WEST_BORDER = -122.517;
+
+function calcX(longitude) {
+    return (longitude - WEST_BORDER) / (EAST_BORDER - WEST_BORDER)
+}
+function calcY(latitude) {
+    return 1 - (latitude - SOUTH_BORDER) / (NORTH_BORDER - SOUTH_BORDER)
+}
+
 async function fetchData() {
+    // Fetch vehicle positions
+    let vehicles;
     try {
-        // Get vehicle positions as pbuf
         const request = new Request("http://localhost:8000/live");
         const response = await fetch(request);
         if (!response.ok) {
             throw new Error(`Response status: ${response.status} ${response.statusText}`);
         }
-        vehiclesDiv.textContent = await response.text();
+        vehicles = await response.json();
     } catch (e) {
         alert(e);
+    }
+    // Draw on canvas
+    if (canvas.getContext) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "rgb(128 0 0)";
+        for (const [_, vehicle] of Object.entries(vehicles)) {
+            let x = calcX(vehicle.longitude) * canvas.width;
+            let y = calcY(vehicle.latitude) * canvas.height;
+            ctx.fillRect(x, y, 2, 2);
+        }
+    } else {
+
     }
 
     // fetch("http://localhost:8000/live")
